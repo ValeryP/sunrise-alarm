abb8a6414171a19a0bf6841bff5d876003d9d583
p_val <palval16@gmail.com>
2020-05-27 20:39:14 +0200

    Comments

--- firebase-cloud-fun/functions/index.js
+++ firebase-cloud-fun/functions/index.js
@@ -1,6 +1,12 @@
 const functions = require('firebase-functions');
 const axios = require("axios");
 
+/**
+ * Ssend a message to the Android app via FCM and switch the light
+ *
+ * @param response object responsible for sending the data
+ * @param isLightOn mode of the switch (lights on, lights off)
+ */
 function sendFCM(response, isLightOn) {
     const postData = {
         "to": "/topics/light",
@@ -26,5 +32,6 @@ function sendFCM(response, isLightOn) {
         })
 }
 
+// Function triggered externally to send a message to the Android app
 exports.send_fcm_light_on = functions.https.onRequest((_, response) => sendFCM(response, true));
 exports.send_fcm_light_off = functions.https.onRequest((_, response) => sendFCM(response, false));
