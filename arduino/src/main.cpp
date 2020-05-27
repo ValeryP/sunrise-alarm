2b7b9a9ea1f22dca196d40065e21aa22f134e7fe
p_val <palval16@gmail.com>
2020-05-27 08:48:54 +0200

    Added comments

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -4,29 +4,43 @@
 #include "GyverTimer.h"
 #include "GyverButton.h"
 
+// LCD display pins
 #define CLK 2
 #define DIO 3
+
+// button for the manual control
 #define BUTTON 4
+
+// relay to control external power 
 #define RELAY 12
 
+// real time clock to keep traking time after reboot
 RTC_DS3231 rtc;
+
+// TM1637 LCD
 GyverTM1637 disp(CLK, DIO);
+
+// button for manual control
 GButton button(BUTTON);
 
+// timers to optimise the monitoring
 GTimer_ms check_serial_input(200);
 GTimer_ms check_alarm_time(1000);
 GTimer_ms check_alarm_state(10);
 
+// state management variables
 int8_t alarm_hrs, alarm_mins;
 boolean is_alarm;
 boolean is_mode_manual_on;
 
+// initializes and sets the initial values
 void setup(){
 	Serial.begin(9600);
 	pinMode(RELAY, OUTPUT);
 	digitalWrite(RELAY, LOW);
 
 	rtc.begin();
+	// re-sync the RTC on start 
 	if (rtc.lostPower()) {
 		rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 	}
@@ -36,6 +50,18 @@ void setup(){
 	disp.point(1);
 }
 
+// loops consecutively
+void loop(){
+	alert_setup_handler();
+	button_monitor_state();
+	// don't use alarm for manual mode (when turned on the light)
+	if (!is_mode_manual_on) { 
+		alert_trigger_handler();
+	}
+	alert_state_monitor();
+}
+
+// checks serial, read alarm time and display it on the LCD screen
 void alert_setup_handler() {
 	if (check_serial_input.isReady()) {
 		if (Serial.available()) {
@@ -48,6 +74,7 @@ void alert_setup_handler() {
 	}
 }
 
+// defined the state of alarm based on the time range
 void alert_trigger_handler(){
 	if (check_alarm_time.isReady()) {
 		DateTime now = rtc.now();
@@ -55,6 +82,7 @@ void alert_trigger_handler(){
 	}
 }
 
+// switch the mode once the button click detected
 void button_monitor_state(){
 	button.tick();
 	if (button.isClick()) {
@@ -63,17 +91,9 @@ void button_monitor_state(){
 	}
 }
 
+// switch the mode once the alarm is triggered
 void alert_state_monitor() {
 	if (check_alarm_state.isReady()) {
 		digitalWrite(RELAY, is_alarm ? HIGH : LOW);
 	}
-}
-
-void loop(){
-	alert_setup_handler();
-	button_monitor_state();
-	if (!is_mode_manual_on) {
-		alert_trigger_handler();
-	}
-	alert_state_monitor();
 }
